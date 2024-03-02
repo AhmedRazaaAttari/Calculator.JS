c7df09df250a34047399b1d32a9c3a5ad287b447
AhmedRazaaAttari <154536166+AhmedRazaaAttari@users.noreply.github.com>
2024-03-02 14:44:29 +0500

    Add files via upload

---
+++ Calculator.js/app.js
@@ -0,0 +1,18 @@
+// Calculator
+let star1 = +prompt("Enter Number 1");
+let star2 = +prompt("Enter Number 2");
+
+let sum = star1 + star2;
+document.write("Sum of number ", sum , "<br/>");
+
+let sub = star1 - star2;
+document.write("Subtraction of number ", sub , "<br/>");
+
+let div = star1 / star2;
+document.write("Divide of number ", div , "<br/>");
+
+let mul = star1 * star2;
+document.write("Multiply of number ", mul , "<br/>");
+
+let Reminder = star1 % star2;
+document.write("Reminder of number ", Reminder , "<br/>");
